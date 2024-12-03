be056e71539498b009dd6330762012f3fd5dcd0f
Raiyan109 <kraiyan109@gmail.com>
2024-12-03 11:50:50 +0600

    ok

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config()
+const sendEmail = require('./utils/sendEmail')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.post('/api/test-email', async (req, res) => {
+    try {
+        await sendEmail({
+            //the client email 
+            to: 'raiyankabir72@gmail.com',
+            //sendGrid sender id 
+            from: 'kraiyan109@gmail.com',
+            subject: 'Does this work?',
+            text: 'Glad you are here .. yes you!',
+            html: '<strong>It is working!!</strong>'
+        });
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e, 'error from sendgrid');
+        res.sendStatus(500);
+    }
+});
 
 // mongoose.set("strictQuery", false);
 // mongoose.connect(process.env.MONGO_URI)
@@ -15,7 +33,7 @@ app.use(cors())
 app.listen(process.env.PORT, (req, res) => {
     console.log(`Server listening on ${process.env.PORT}`)
 })
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
